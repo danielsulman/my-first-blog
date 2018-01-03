35040a1639e923b32c988bbe4cd4e769235b4d81
Daniel <danielsulman_7@hotmail.com>
2018-01-03 18:15:04 -0200

    alteracao no css, template, views e urls

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,get_list_or_404
+from django.shortcuts import render,get_list_or_404,get_object_or_404
 from .models import Post
 
 # Create your views here.
@@ -7,3 +7,7 @@ from .models import Post
 def post_list(request):
     posts = get_list_or_404(Post)
     return render(request,'blog/post_list.html',{'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request,'blog/post_detail.html',{'post': post})
