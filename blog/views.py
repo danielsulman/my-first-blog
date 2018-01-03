573f82f6acf9383fccc244b31dff1aee5ee919fc
Daniel <danielsulman_7@hotmail.com>
2018-01-03 16:41:39 -0200

    edicao html, views, url template

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_list_or_404
+from .models import Post
 
 # Create your views here.
+
+
+def post_list(request):
+    posts = get_list_or_404(Post)
+    return render(request,'blog/post_list.html',{'posts': posts})
