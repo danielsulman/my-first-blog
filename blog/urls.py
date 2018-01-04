d9e6f17e005af5f95ef6390ade44c92a0c75e0c6
Daniel <danielsulman_7@hotmail.com>
2018-01-04 16:36:15 -0200

    Login working

--- blog/urls.py
+++ blog/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
     path('drafts/', views.post_draft_list, name='post_draft_list'),
     path('post/<int:pk>/publish', views.post_publish, name='post_publish'),
     path('post/<int:pk>/delete', views.post_remove, name='post_remove'),
-    path('post/<int:pk>/edit', views.post_edit, name='post_edit')
+    path('post/<int:pk>/edit', views.post_edit, name='post_edit'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -16,7 +16,7 @@ def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request,'blog/post_detail.html',{'post': post})
 
-
+@login_required
 def post_new(request):
     if request.method == 'POST':
         form = PostForm(request.POST)
@@ -30,22 +30,25 @@ def post_new(request):
 
     return render(request, 'blog/post_new.html', {'form': form})
 
+@login_required
 def post_draft_list(request):
     posts = get_list_or_404(Post, published_date__isnull=True, author=request.user)
 
     return render(request, 'blog/post_draft_list.html', {'posts': posts})
 
+@login_required
 def post_publish(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.publish()
     return redirect('blog:post_detail', pk=pk)
 
+@login_required
 def post_remove(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.delete()
     return redirect('blog:post_list')
 
-
+@login_required
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == 'POST':
