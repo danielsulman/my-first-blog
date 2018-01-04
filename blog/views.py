14acaadc2a9389b91472bb2cf04ea27614a67645
Daniel <danielsulman_7@hotmail.com>
2018-01-04 11:13:30 -0200

    adicao da criacao de posts na url new

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,get_list_or_404,get_object_or_404
 from .models import Post
+from .forms import PostForm
 
 # Create your views here.
 
@@ -8,6 +9,12 @@ def post_list(request):
     posts = get_list_or_404(Post)
     return render(request,'blog/post_list.html',{'posts': posts})
 
+
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request,'blog/post_detail.html',{'post': post})
+
+
+def post_new(request):
+    form = PostForm()
+    return render(request, 'blog/post_new.html', {'form': form})
